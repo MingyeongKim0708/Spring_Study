e95b3a8dad665b09ae6da4b6e28968c548c1c3b8
Mingyeong Kim <kimmg0708@naver.com>
2024-11-01 17:44:18 +0900

    schema sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,10 +20,36 @@ CREATE TABLE IF NOT EXISTS `curriculum` (
     name VARCHAR(100) NOT NULL
 );
 
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE IF NOT EXISTS `users`(
-    id INT PRIMARY KEY,
+    id VARCHAR(100) PRIMARY KEY,
     password VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
     curriculum_code INT,
     FOREIGN KEY (curriculum_code) REFERENCES curriculum(code)
 );
+
+SELECT * FROM board;
+
+SELECT * FROM curriculum;
+
+SELECT * FROM users;
+
+INSERT INTO `ssafy_study`.`curriculum` (`code`,`name`)
+VALUES (100, "Python"), (200, "Java"), (300, "C");
+
+
+INSERT INTO users
+VALUES ("ssafy", "1234", "김싸피", 200),
+("admin", "1234", "admin", 200);
+-- admin '이름'일 때 관리자 페이지로 넘어가게 해놨음
+
+
+INSERT INTO users
+VALUES ("study", "1234", "스터디", 100);
+
+
+-- 기타
+
+UPDATE curriculum set name = "Python" WHERE code = 100;
